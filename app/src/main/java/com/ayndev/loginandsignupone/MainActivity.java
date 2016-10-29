44cf13e59e1e5b5caea75ba5f5e9d330ac67b1e7
ayinmursalin <ayinmursalin12@gmail.com>
2016-10-29 22:09:17 +0700

    adding menu to MainActivity

--- app/src/main/java/com/ayndev/loginandsignupone/MainActivity.java
+++ app/src/main/java/com/ayndev/loginandsignupone/MainActivity.java
@@ -6,7 +6,10 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,4 +31,21 @@ public class MainActivity extends AppCompatActivity {
       });
    }
 
+   @Override
+   public boolean onCreateOptionsMenu(Menu menu) {
+      getMenuInflater().inflate(R.menu.menu_main, menu);
+      return true;
+   }
+
+   @Override
+   public boolean onOptionsItemSelected(MenuItem item) {
+
+      switch (item.getItemId()) {
+         case R.id.menu_action_setting :
+            Toast.makeText(this, "Settings Activity", Toast.LENGTH_LONG).show();
+            break;
+      }
+
+      return super.onOptionsItemSelected(item);
+   }
 }
